d309caaf6b3d7cd3f669d3f9de7138f3d1f0431f
Rafael <rafaelmoura7033@gmail.com>
2022-04-19 18:41:39 -0300

    Criando a migration da tabela Matches

---
+++ app/backend/src/database/migrations/20220417194224-create-table-matches.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Matches', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      home_team: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Teams',
+          key: 'id',
+        },
+      },
+      home_team_goals: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      away_team: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Teams',
+          key: 'id',
+        },
+      },
+      away_team_goals: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    return queryInterface.dropTable('Matches');
+  }
+};
