56748e553eb2f0a1fe7d684a830d8a87c6e43175
Rafael <rafaelmoura7033@gmail.com>
2022-04-23 19:53:45 -0300

    Consertando assinatura JWT

--- app/backend/src/services/User.ts
+++ app/backend/src/services/User.ts
@@ -3,7 +3,7 @@ import Login from '../interfaces/Login';
 
 export default class UserService {
   static login(data: Login): string {
-    const token = jwt.sign(data, 'dale', { expiresIn: '7d', algorithm: 'HS256' });
+    const token = jwt.sign({ ...data }, 'dale', { expiresIn: '7d', algorithm: 'HS256' });
     return token;
   }
 }
