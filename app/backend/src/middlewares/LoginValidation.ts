38262670c92f93017a76aff0d05c9831e9be8d0a
Rafael <rafaelmoura7033@gmail.com>
2022-04-23 19:54:23 -0300

    Criando a classe de validação de login

---
+++ app/backend/src/middlewares/LoginValidation.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from 'express';
+import User from '../database/models/User';
+
+const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+
+export default class LoginValidation {
+  static async emailValidation(req: Request, _res: Response, next: NextFunction) {
+    const { email } = req.body;
+    if (!email) return next({ status: 400, message: 'O campo email é obrigatório' });
+    if (!emailRegex.test(email)) {
+      return next(
+        { status: 400, message: 'Email inválido' },
+      );
+    }
+    const result = await User.findOne({ where: email });
+    if (!result) return next({ status: 404, message: 'Email e/ou senha incorreto(s)' });
+    req.body.user = result;
+    return next();
+  }
+
+  static async passwordValidation(req: Request, _res: Response, next: NextFunction) {
+    const { password, user: { password: rightPassword } } = req.body;
+    if (!password) return next({ status: 400, message: 'O campo password é obrigatório' });
+    if (password !== rightPassword) {
+      return next(
+        { status: 400, message: 'Email e/ou senha incorreto(s)' },
+      );
+    }
+    return next();
+  }
+}
