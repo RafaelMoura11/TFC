c2efd3234b3022cbeec5789878e1737bbedf33e5
Rafael <rafaelmoura7033@gmail.com>
2022-04-23 18:59:31 -0300

    Mudando nome da rota

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,11 +1,11 @@
 import { App } from './app';
 import 'dotenv/config';
-import loginRouter from './routes/User';
+import userRouter from './routes/User';
 
 const PORT = process.env.PORT || 3001;
 
 const app = new App();
 
-app.use('/login', loginRouter);
+app.use('/login', userRouter);
 
 app.start(PORT);
