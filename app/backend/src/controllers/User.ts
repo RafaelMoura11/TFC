39ccb731bcc7b996173227f44995d62b8fe1b466
Rafael <rafaelmoura7033@gmail.com>
2022-04-23 19:54:11 -0300

    Ajustando retorno do atributo estatico login

--- app/backend/src/controllers/User.ts
+++ app/backend/src/controllers/User.ts
@@ -3,7 +3,7 @@ import Login from '../interfaces/Login';
 import UserService from '../services/User';
 
 export default class UserController {
-  static async login(req: Request, res: Response) {
+  static async login(req: Request, res: Response): Promise<Response> {
     const token = UserService.login(req.body as Login);
     return res.status(200).json({ token });
   }
