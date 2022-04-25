dd1f6289167b234eaa2f9da870499f3aa5b959df
Rafael <rafaelmoura7033@gmail.com>
2022-04-25 11:56:59 -0300

    Importando modulo fs

--- app/backend/src/services/User.ts
+++ app/backend/src/services/User.ts
@@ -1,5 +1,9 @@
 import jwt = require('jsonwebtoken');
+// import fs = require('fs');
+// import path = require('path');
 import Login from '../interfaces/Login';
+
+// const SECRET = fs.readFileSync(path.join('../jwt.evaluation.key'));
 
 export default class UserService {
   static login(data: Login): string {
