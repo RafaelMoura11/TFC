65350405285a186e848a5282b7a5f1ce7d6fbbef
Rafael <rafaelmoura7033@gmail.com>
2022-04-25 10:33:00 -0300

    Adicionando validações a rota

--- app/backend/src/routes/User.ts
+++ app/backend/src/routes/User.ts
@@ -1,8 +1,16 @@
 import express = require('express');
+import LoginValidation from '../middlewares/LoginValidation';
 import LoginController from '../controllers/User';
 
 const loginRouter = express.Router();
 
-loginRouter.post('/', LoginController.login);
+loginRouter.post(
+  '/',
+  LoginValidation.emailValidation,
+
+  LoginValidation.passwordValidation,
+
+  LoginController.login,
+);
 
 export default loginRouter;
