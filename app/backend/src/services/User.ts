e1617a038f0e20f2d056997f60baed955539ee67
Rafael <rafaelmoura7033@gmail.com>
2022-04-25 18:25:00 -0300

    Mudando o secret

--- app/backend/src/services/User.ts
+++ app/backend/src/services/User.ts
@@ -7,7 +7,7 @@ import Login from '../interfaces/Login';
 
 export default class UserService {
   static login(data: Login): string {
-    const token = jwt.sign({ ...data }, 'dale', { expiresIn: '7d', algorithm: 'HS256' });
+    const token = jwt.sign({ ...data }, 'super_senha', { expiresIn: '7d', algorithm: 'HS256' });
     return token;
   }
 }
