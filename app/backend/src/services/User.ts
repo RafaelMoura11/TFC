403cb7a96510e1adaa609120772aa9c7be370a56
Rafael <rafaelmoura7033@gmail.com>
2022-04-25 18:28:34 -0300

    Implementando secret com modulo fs

--- app/backend/src/services/User.ts
+++ app/backend/src/services/User.ts
@@ -1,13 +1,12 @@
 import jwt = require('jsonwebtoken');
-// import fs = require('fs');
-// import path = require('path');
+import fs = require('fs');
 import Login from '../interfaces/Login';
 
-// const SECRET = fs.readFileSync(path.join('../jwt.evaluation.key'));
+const SECRET = fs.readFileSync('jwt.evaluation.key').toString('utf8');
 
 export default class UserService {
   static login(data: Login): string {
-    const token = jwt.sign({ ...data }, 'super_senha', { expiresIn: '7d', algorithm: 'HS256' });
+    const token = jwt.sign({ ...data }, SECRET, { expiresIn: '7d', algorithm: 'HS256' });
     return token;
   }
 }
