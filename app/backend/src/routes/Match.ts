7c1514ace8e57a951d56a4fd7f7575988cc13626
Rafael <rafaelmoura7033@gmail.com>
2022-04-28 11:59:00 -0300

    Adicionando validação de JWT na rota /matches

--- app/backend/src/routes/Match.ts
+++ app/backend/src/routes/Match.ts
@@ -1,10 +1,11 @@
 import express = require('express');
+import LoginValidation from '../middlewares/LoginValidation';
 import MatchController from '../controllers/Match';
 
 const matchRouter = express.Router();
 
 matchRouter.get('/:inProgress', MatchController.getMatches);
-matchRouter.post('/', MatchController.createNewMatch);
+matchRouter.post('/', LoginValidation.jwtValidation, MatchController.createNewMatch);
 matchRouter.patch('/:id/finish', MatchController.finishMatch);
 
 export default matchRouter;
