5f08f0b179db70db0da2752566c771d023063cf7
Rafael <rafaelmoura7033@gmail.com>
2022-04-28 21:59:33 -0300

    Separando rotas de buscar matches

--- app/backend/src/controllers/Match.ts
+++ app/backend/src/controllers/Match.ts
@@ -3,14 +3,15 @@ import MatchService from '../services/Match';
 import MatchBody from '../interfaces/Match';
 
 export default class MatchController {
-  static async getMatches(req: Request, res: Response): Promise<Response> {
-    if (req.query.inProgress) {
-      const isInProgressTrue = req.query.inProgress === 'true';
-      const matchesInProgress = await MatchService.getMatchesByInProgress(isInProgressTrue);
-      return res.status(200).json(matchesInProgress);
-    }
+  static async getMatches(_req: Request, res: Response): Promise<Response> {
     const matches = await MatchService.getMatches();
     return res.status(200).json(matches);
+  }
+
+  static async getMatchesByInProgress(req: Request, res: Response): Promise<Response> {
+    const isInProgressTrue = req.query.inProgress === 'true';
+    const matchesInProgress = await MatchService.getMatchesByInProgress(isInProgressTrue);
+    return res.status(200).json(matchesInProgress);
   }
 
   static async createNewMatch(req: Request, res: Response, next: NextFunction):
--- app/backend/src/routes/Match.ts
+++ app/backend/src/routes/Match.ts
@@ -4,7 +4,8 @@ import MatchController from '../controllers/Match';
 
 const matchRouter = express.Router();
 
-matchRouter.get('/:inProgress', MatchController.getMatches);
+matchRouter.get('/:inProgress', MatchController.getMatchesByInProgress);
+matchRouter.get('/', MatchController.getMatches);
 matchRouter.post('/', LoginValidation.jwtValidation, MatchController.createNewMatch);
 matchRouter.patch('/:id/finish', MatchController.finishMatch);
 
