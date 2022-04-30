9142d6ccb36f32a8cc0f72670b017568c2c8380e
Rafael <rafaelmoura7033@gmail.com>
2022-04-30 16:53:00 -0300

    Separando a funcionalidade em um middleware

--- app/backend/src/controllers/Match.ts
+++ app/backend/src/controllers/Match.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import MatchService from '../services/Match';
 import MatchBody from '../interfaces/Match';
 
@@ -14,19 +14,17 @@ export default class MatchController {
     return res.status(200).json(matchesInProgress);
   }
 
-  static async createNewMatch(req: Request, res: Response, next: NextFunction):
+  static async createNewMatch(req: Request, res: Response):
   Promise<Response | void> {
     const newMatch = req.body as MatchBody;
-    if (newMatch.awayTeam === newMatch.homeTeam) {
-      return next({ status: 400,
-        message: 'It is not possible to create a match with two equal teams' });
-    }
+    console.log(newMatch);
     const createdMatch = await MatchService.createNewMatch(newMatch);
     return res.status(200).json(createdMatch);
   }
 
   static async finishMatch(req: Request, res: Response): Promise<Response> {
     const { id } = req.params;
+    console.log(req.body);
     await MatchService.finishMatch(id);
     return res.status(200).json('Game Over!');
   }
---
+++ app/backend/src/middlewares/NewMatch.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from 'express';
+import MatchBody from '../interfaces/Match';
+
+const isTheSameTeam = async (req: Request, _res: Response, next: NextFunction) => {
+  const newMatch = req.body as MatchBody;
+  if (newMatch.awayTeam === newMatch.homeTeam) {
+    return next({ status: 400,
+      message: 'It is not possible to create a match with two equal teams' });
+  }
+  return next();
+};
+
+export default isTheSameTeam;
