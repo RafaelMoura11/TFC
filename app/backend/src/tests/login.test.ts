9418a91ef5dafbf275178ba465712e1c14578ab4
RafaelMoura11 <rafaelmoura7033@gmail.com>
2022-05-11 13:36:40 -0300

    Adicionando inicio de testes em login

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,44 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import User from '../database/models/User';
+
+import { Response, Request } from 'express';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+
+describe('Testando rota de Login', () => {
+  beforeEach(async () => {
+    sinon.stub(User, 'findOne')
+  })
+  afterEach(() => {
+    (User.findOne as sinon.SinonStub).restore()
+  })
+
+  it('Deve retornar um erro 400 caso o email esteja vazio', () => {
+    const user = { email: '', password: '123456' };
+    chai.request(app)
+    .post('login')
+    .send(user)
+    .end((_err, res) => {
+      res.should.have.status(400);
+      res.body.should.have.property('message')
+    })
+  })
+  it('Deve retornar um erro 400 caso o password esteja vazio', () => {
+    (User.findOne as sinon.SinonStub).resolves(true);
+    const user = { email: 'test@test.com', password: '' };
+    chai.request(app)
+    .post('login')
+    .send(user)
+    .end((_err, res) => {
+      res.should.have.status(400);
+      res.body.should.have.property('message')
+    })
+  })
+});
